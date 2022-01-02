b88ab887a68436d4ab86c957af23f4312d0fd160
dandisatrio <dandisatrio810@gmail.com>
2022-01-02 15:19:01 +0700

    local build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  reactStrictMode: true,
   images: {
     domains: ["http://localhost:4000"],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
